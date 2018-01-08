08524c0ea8b843bb10f0445ff083fc2c94251e0f
Feynman Liang <feynman.liang@gmail.com>
2018-01-07 19:36:50 -0800

    Traversable implementation, kinda slow

--- src/main/scala/learnfp/traversable/Traversable.scala
+++ src/main/scala/learnfp/traversable/Traversable.scala
@@ -7,8 +7,13 @@ class TraversableOps[A, F[_]](initialXs:List[F[A]])(implicit functor: Functor[F]
   import learnfp.functor.FunctorOps._
   import learnfp.applicative.ApplicativeOps._
 
-  def traverse[B](fx:A => B)(implicit applicative: Applicative[F]):F[List[B]] = ???
-  def sequence(implicit applicative: Applicative[F]):F[List[A]] = ???
+  def traverse[B](fx:A => B)(implicit applicative: Applicative[F]):F[List[B]] =
+    initialXs.foldRight(
+      applicative.pure(List.empty[B])
+    ) { (appx, acc) =>
+      (((b:B) => (bs:List[B]) => b::bs) `<$>` (fx `<$>` appx)) <*> acc
+    }
+  def sequence(implicit applicative: Applicative[F]):F[List[A]] = traverse(x => x)
 }
 
 object TraversableOps {
