a88cf199fa08513786efef98f0dcc47fa3fb8b58
Feynman Liang <feynman.liang@gmail.com>
2018-01-16 20:18:10 -0800

    Cofunctor

--- src/main/scala/learnfp/cofunctor/Show.scala
+++ src/main/scala/learnfp/cofunctor/Show.scala
@@ -4,6 +4,6 @@ case class Show[A](show:A => String)
 
 object Show {
   implicit val showCoFunctorInstance = new CoFunctor[Show] {
-    override def cmap[A, B](b: Show[B])(fx: A => B): Show[A] = ???
+    override def cmap[A, B](b: Show[B])(fx: A => B): Show[A] = Show(a => b.show(fx(a)))
   }
 }
